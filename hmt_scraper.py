894e2b299f763b3651d68b05b93e257e6d950a0c
zensquirrel <emil.michels@posteo.de>
2023-06-09 09:29:13 +0200

    Error handling in scraping process

--- hmt_scraper.py
+++ hmt_scraper.py
@@ -6,13 +6,16 @@ from bs4 import BeautifulSoup
 def get_wartenummer():
     proxies, headers = fake_profile_generator()
     url = 'https://www.hmt-rostock.de/storages/hmt-rostock/service/wartenummer.php'
-    r = requests.get(url,
-                     proxies=proxies,
-                     headers=headers,
-                     timeout=5)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    wartenummer = soup.body.contents[0]
-    return wartenummer.strip()
+    try:
+        r = requests.get(url,
+                         proxies=proxies,
+                         headers=headers,
+                         timeout=5)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        wartenummer = soup.body.contents[0]
+        return wartenummer.strip()
+    except ConnectionError:
+        print('Could not connect to the HMT servers. They are probably down.')
 
 
 if __name__ == "__main__":
